eca8f75a205f1fccf9a56f0f40ea950d296720e8
Stephen Marquard <stephen.marquard@uct.ac.za>
2018-02-07 13:31:25 +0200

    OPENCAST-1979 incorporates changes from Devan's update and added additional stats

--- detectTrimPoints_woh.py
+++ detectTrimPoints_woh.py
@@ -18,6 +18,7 @@ from pyAudioAnalysis import audioSegmentation as aS
 import itertools as it
 import wave
 import contextlib
+import time
 
 def usage():
     print "This script requires an audio file as a parameter"
@@ -29,10 +30,16 @@ if len(sys.argv) > 1:
     inputWavFile = sys.argv[1]
     outputTextFile = sys.argv[2]
 
+    speech  = 1 # ALWAYS
+    non_speech = sys.argv[3] 
+
+    threshold = int(sys.argv[4]) # 1:30
+    start_time = time.time() 	
+
     if not os.path.isfile(inputWavFile):
         print "Cannot locate audio file " + inputWavFile
 
-    modelName = "model/svmFinalAll"
+    modelName = "model/svmModel"
     if not os.path.isfile(modelName):
         print "Cannot locate model file " + modelName
 
@@ -48,6 +55,7 @@ if len(sys.argv) > 1:
     reversed_segs = segs[::-1]
     reversed_classes = classes[::-1]
 
+    # get the duration of the wave file
     audio_trim_duration = 0
     with contextlib.closing(wave.open(inputWavFile,'r')) as f:
         frames = f.getnframes()
@@ -55,23 +63,28 @@ if len(sys.argv) > 1:
         duration = frames / float(rate)
         audio_trim_duration = int(duration*1000)
 
-    speech  = 1 
-    chatter = 0 # remove chatter
-    other   = 0 # remove other
-    threshold = 90 # 1:30
-
     output_arr = []
     step = "none"
     segment_prev = 0
-   
+    
+    stats = [0,0, 0,0] #numpy.zeros((2, 2))
+
+
     #print "\n"
     for s in range(len(segs)):
 	segment = segs[s]
         diff = int(segment[1]) - int(segment[0])
 	#print str(int(segment[0])*1000) +"-"+ str(int(segment[1])*1000)  + " : " + str(classes[s]) + " (" + str(diff) + ")\n"
 
-	if ((chatter == 0) and (str(classes[s]) == "chatter") and (diff >= threshold)) or \
-	   ((other == 0) and (str(classes[s]) == "other") and (diff >= threshold)):
+        if (str(classes[s]) == "speech"):
+            stats[0] += 1
+            stats[1] += diff        
+
+        if (str(classes[s]) == "non-speech"):
+            stats[2] += 1
+            stats[3] += diff
+
+	if (non_speech == 0) and (str(classes[s]) == "non-speech") and (diff >= threshold):
 		#print str(int(segment[0])*1000) +"-"+ str(int(segment[1])*1000)  + " : " + str(classes[s]) + " (" + str(diff) + ")\n"
 		segment_start = int(segment[0])*1000
 		segment_end = int(segment[1])*1000
@@ -102,48 +115,36 @@ if len(sys.argv) > 1:
     if (len(output_arr) == 0):
 	output_arr.append(0)
 	output_arr.append(audio_trim_duration);
-
     '''
     print('audio_trim_file=' + inputWavFile +'\n')
     print('audio_trim_out_file=' + outputTextFile +'\n')
     print('audio_trim_duration=' + str(audio_trim_duration) +'\n')
     print('audio_trim_segments=' + str(';'.join(str(e) for e in output_arr)) +'\n')
-
-    print(str(len(output_arr)) +'\n')
+    print('audio_trim_segments_no=' + str(len(segs)) +'\n')
+    print('audio_trim_segments_speech_no=' + str(int(stats[0])) +'\n')
+    print('audio_trim_segments_speech_ms=' + str(int(stats[1]) * 1000) +'\n')
+    print('audio_trim_segments_notspeech_no=' + str(int(stats[2])) +'\n')
+    print('audio_trim_segments_notspeech_ms=' + str(int(stats[3]) * 1000) +'\n')
+    print("audio_trim_exec_time=%s\n" % round((time.time() - start_time), 3))
     '''
+    #print(stats)
+    #print(str(len(output_arr)) +'\n')
+    
 
     f = open(outputTextFile, 'w')
-    f.write('audio_trim_file=' + inputWavFile +'\n')
-    f.write('audio_trim_out_file=' + outputTextFile +'\n')
+    #f.write('audio_trim_file=' + inputWavFile +'\n')
+    #f.write('audio_trim_out_file=' + outputTextFile +'\n')
     f.write('audio_trim_duration=' + str(audio_trim_duration) +'\n')
     f.write('audio_trim_segments=' + str(';'.join(str(e) for e in output_arr)) +'\n')
+    f.write('audio_trim_segments_no=' + str(len(segs)) +'\n')
+    f.write('audio_trim_segments_speech_no=' + str(int(stats[0])) +'\n')
+    f.write('audio_trim_segments_speech_ms=' + str(int(stats[1]) * 1000) +'\n')
+    f.write('audio_trim_segments_notspeech_no=' + str(int(stats[2])) +'\n')
+    f.write('audio_trim_segments_notspeech_ms=' + str(int(stats[3]) * 1000) +'\n')
+    f.write("audio_trim_exec_time=%s\n" % round((time.time() - start_time), 3))
     f.close()
 
-    #print("audio_trim_file=" + inputWavFile)
-
-    '''
-    output = "{"
-
-    for s in range(len(segs)):
-        segment = segs[s]
-        diff = int(segment[1]) - int(segment[0])
-        if str(classes[s]) == "speech" and diff > 3:
-            startpoint = segs[s]
-            #print "start trim: " + str(int(startpoint[0]*1000))
-            output = output + "'start':'" + str(int(startpoint[0]*1000)) + "','"
-            break
-
-    for s in range(len(reversed_segs)):
-        segment = reversed_segs[s]
-        diff = int(segment[1]) - int(segment[0])
-        if str(reversed_classes[s]) == "speech" and diff > 5:
-            endpoint = reversed_segs[s]
-            #print "end trim: " + str((int(endpoint[1]) + 10)*1000)
-            output = output + "end':'" + str((int(endpoint[1]) + 10)*1000) + "'}"
-            break
-    
-    #print output
-    
+'''
 else:
     usage()
     sys.exit(os.EX_USAGE)
